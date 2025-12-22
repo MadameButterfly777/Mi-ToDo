6195fbdf6154711e0f7cc36a47ae8fda1ca22976
Georgina Solange <georginasolbarroso@gmail.com>
2025-12-22 16:08:20 -0300

    cree el componente taskList

---
+++ mi-todo/src/TaskList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import TaskItem from "./TaskItem";
+
+export default function TaskList({ tasks, onToggle, onDelete, onEdit }) {
+  if (!tasks.length)
+    return <p className="has-text-centered has-text-grey-light mt-5">No hay estrellas que marcar aún...</p>;
+
+  return (
+    <div>
+      {tasks.map((task, i) => (
+        <TaskItem
+          key={i}
+          task={task}
+          index={i}
+          onToggle={onToggle}
+          onDelete={onDelete}
+          onEdit={onEdit}
+        />
+      ))}
+    </div>
+  );
+}
